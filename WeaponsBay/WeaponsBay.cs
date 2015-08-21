84163521274c3f45466f9f080e0d8d2d109bda01
seamus0riley <clown.baby244@gmail.com>
2015-08-20 23:02:05 -0400

    Added comments to the weaponsbay code

--- WeaponsBay/WeaponsBay.cs
+++ WeaponsBay/WeaponsBay.cs
@@ -13,9 +13,9 @@ namespace WeaponsBay
 
 
         [KSPField(isPersistant = true)]
-        public bool isOpen = false;
+        public bool isOpen = false; //door starts closed
 
-        public float degreesPerSecond = 1f;
+        public float degreesPerSecond = 1f; 
         public float totalRotation = 0;
         public float rotationAmount = 15f;
         private MissileFire weaponManager;
@@ -28,7 +28,7 @@ namespace WeaponsBay
 
 
 
-        [KSPAction("Open")]
+        [KSPAction("Open")] //for action group use
         public void Open(KSPActionParam param)
         {
            OpenBay();
@@ -36,7 +36,7 @@ namespace WeaponsBay
 
        
 
-        [KSPAction("Close")]
+        [KSPAction("Close")] //for action group use
         public void Close(KSPActionParam param)
         {
             CloseBay();
@@ -49,9 +49,9 @@ namespace WeaponsBay
             
             isOpen = true;
 
-
-            Quaternion rotation = Quaternion.AngleAxis(120, part.transform.forward);
-            part.transform.localRotation = Quaternion.Slerp(part.transform.rotation, rotation, .05f);
+            //Rotation nonsense from before
+            //Quaternion rotation = Quaternion.AngleAxis(120, part.transform.forward);
+            //part.transform.localRotation = Quaternion.Slerp(part.transform.rotation, rotation, .05f);
 
 
             // transform.localEulerAngles = new Vector3(10, 0, 0);
@@ -69,8 +69,10 @@ namespace WeaponsBay
         {
 
             isOpen = false;
-            Quaternion rotation = Quaternion.AngleAxis(-120, part.transform.forward);
-            part.transform.localRotation = Quaternion.Slerp(part.transform.rotation, rotation, .05f);
+
+            //Rotation nonsense from before
+            //Quaternion rotation = Quaternion.AngleAxis(-120, part.transform.forward);
+           // part.transform.localRotation = Quaternion.Slerp(part.transform.rotation, rotation, .05f);
 
             Toggle("OpenBay", true);
             Toggle("CloseBay", false);
@@ -82,11 +84,11 @@ namespace WeaponsBay
 
         public override void OnStart(StartState state)
         {
-            var rotation = part.transform.rotation;
+          
 
             if (state != StartState.Editor)
             {
-                foreach (var wm in vessel.FindPartModulesImplementing<MissileFire>())
+                foreach (var wm in vessel.FindPartModulesImplementing<MissileFire>()) //make sure a weapons manager exists on the vessel after launch
                 {
                     weaponManager = wm;
 
@@ -95,7 +97,7 @@ namespace WeaponsBay
             }
         }
 
-        private void Toggle(string eventName, bool state)
+        private void Toggle(string eventName, bool state) //toggle gui buttons
         {
             Events[eventName].active = state;
             Events[eventName].externalToEVAOnly = state;
@@ -118,13 +120,13 @@ namespace WeaponsBay
                
 
 
-            if (HighLogic.LoadedSceneIsFlight)
+            if (HighLogic.LoadedSceneIsFlight) //if it's not in the editor
 
 
               
 
             {
-                if (weaponManager.radar && weaponManager.radar.locked && weaponManager.radar.lockedTarget.exists)
+                if (weaponManager.radar && weaponManager.radar.locked && weaponManager.radar.lockedTarget.exists) //make sure it has a radar, it's locked, and it has a valid target
                 {
                     if (!isOpen)
                     {
@@ -132,7 +134,7 @@ namespace WeaponsBay
                         OpenBay();
                     }
                 }
-                else if (weaponManager.isArmed && !weaponManager.radar.locked )
+                else if (!weaponManager.radar.locked ) //if the radar isn't locked close the bay door
                                    
                     {
                     if (isOpen){
